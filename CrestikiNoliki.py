fd2930ef733d2ab8be55b411bd3f11cb39b52cee
Lawr1d <galkinmaksim666@mail.ru>
2022-09-24 16:02:31 +0300

    KN

--- CrestikiNoliki.py
+++ CrestikiNoliki.py
@@ -1,3 +1,4 @@
+from operator import truediv
 import random
 def DisplayBoard(board):
     print('''
@@ -26,4 +27,45 @@ def WhoGoesPerv():
     random.randint()
 
 print(DisplayBoard([' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']))
-print(Proverka())   
+print(Proverka())
+
+def board(x):
+    x = []
+    for i in board:
+        x.append(i)
+    return x
+
+def ProvVybora():
+    while move not in '1 2 3 4 5 6 7 8 9'.split()or not iSF(board,int(move)):
+        print('Ваш следующий ход? Введите номер ячейки. (1-9)')
+        move = input()
+    return int(move)
+
+def ii(board,movesList):
+    possibleMoves = []
+    for i in movesList:
+        if iSF(board,i):
+            possibleMoves.append(i)
+
+    if len(possibleMoves) != 0:
+        return random.choice(possibleMoves)
+    else:
+        return None
+
+board = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' '] 
+board = [' ']*10
+mL = [1,3,7,9]
+
+hod = ii(board,mL)
+print(hod)
+
+DisplayBoard(board)
+
+board[1] = 'X'
+board[7] = 'X'
+board[9] = 'X'
+
+hod = ii(board,mL)
+print(hod)
+
+DisplayBoard(board)
