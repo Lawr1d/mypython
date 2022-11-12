a57e60766eddc6fd1ad505c528cb17ea49790814
Lawr1d <galkinmaksim666@mail.ru>
2022-11-12 15:48:23 +0300

    ponyatno

--- poisk.py
+++ poisk.py
@@ -11,4 +11,43 @@ def getPlayPole():
                 board[x].append('`')
     return board
 
-print(getPlayPole())
+def displayBoard(board):
+    strOne = '    '
+    for i in range(1,6):
+        strOne += ' '*9 + str(i)
+
+    print(strOne)
+    print('   '+('0123456789'*6))
+    print()
+
+    for stroka in range(15):
+        if stroka<10:
+            strPol = ' '
+        else:
+            strPol = ''
+
+        strBoard = ''
+        for stalbec in range(60):
+            strBoard += board[stalbec][stroka]
+
+        print('%s%s %s %s' % (strPol, stroka, strBoard, stroka))
+
+
+    print()
+    print('   '+('0123456789'*6))
+    print(strOne)
+
+def getRandomChests(kolChests):
+    chasts = []
+    while len(chasts)<=kolChests:
+        newChasts = [random.randint(0,59),random.randint(0,14)]
+        if newChasts not in chasts:
+            chasts.append(newChasts)
+    return chasts
+        
+
+            
+
+
+theBoard = getPlayPole()
+displayBoard(theBoard)
