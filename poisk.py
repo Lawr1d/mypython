37a04ea15e0511275d527cdaa3844a39eb9d1ff5
Lawr1d <galkinmaksim666@mail.ru>
2022-12-10 15:42:35 +0300

    gg

--- poisk.py
+++ poisk.py
@@ -1,5 +1,6 @@
 import random
 import math
+import sys
 
 def getPlayPole():
     board = []
@@ -52,6 +53,7 @@ def isOnBoard(x,y):
     return x>=0 and x<=59 and y>=0 and y<=14
 
 def vopros():
+    print('Не хотите закончить проверку?')
     while True:
         otvet = input().lower()
         if (otvet == 'да') or (otvet == 'д') or (otvet == 'yes') or (otvet == 'ye') or (otvet == 'y'):
@@ -64,14 +66,58 @@ def vopros():
 
 def makeMove(board,chasts,x,y):
     minDisrance = 100
-#    for cx,cy in chasts:
+    for cx,cy in chasts:
+        distanciya = math.sqrt((cx-x)*(cx-x)+(cy-y)*(cy-y))
+        if distanciya < minDisrance:
+            minDisrance = distanciya
+    minDisrance = round(minDisrance)
+
+    if minDisrance == 0:
+        chasts.remove(x,y)
+        return 'Вы нашли сундук с сокровищами на затонувшем корабле'
+    else:
+        if minDisrance < 10:
+            board[x][y] = str(minDisrance)
+            return 'Сундук с сокровищами обнаружен на расcтоянии %s единиц от гидролокатора.' % (minDisrance)
+        else:
+            board[x][y] = 'X'
+            return 'Гидролокатор ничего не обнаружил. Все сундуки с сокровищами вне пределов досягаемости'
+def enterPlayerMove(predHoda):
+    print('''Где следует опустить гидролокатор?
+    (координаты: 0-59 0-14)
+    (или набирите "Выход" для прекращения игры''')
+    while True:
+        move = input()
+        if move.lower() == 'выход':
+            print('Спасибо за игру!')
+            sys.exit()
+
+        move = move.split()
+
+        if len(move) == 2 and move[0].isdigit() and move[1].isdigit() and isOnBoard(int(move[0]),int(move[1])):
+            if [int(move[0]),int(move[1])] in predHoda:
+                print('Вы уже опускали сюда гидролокатор')
+            else:
+                return [int(move[0]),int(move[1])]
+        else:
+            print('Введите число от 0 до 59, потом пробел, а затем число от 0 до 14.')
 # ************************************************
 # ОСНОВНОЕ ТЕЛО ПРОГРАММЫ
 # ************************************************
+spis = [[4,3],[13,8]]
+hod = enterPlayerMove(spis)
+print(hod)
 
-while True:
-    a = int(input('Введите первую координату:     '))
-    b = int(input('Введите вторую координату:     '))
-    print(isOnBoard(a,b))
-    if vopros('Не хотите закончить проверку?'):
-        break
+
+
+
+
+
+
+
+#while True:
+#    a = int(input('Введите первую координату:     '))
+#    b = int(input('Введите вторую координату:     '))
+#    print(isOnBoard(a,b))
+#    if vopros():
+#        break
